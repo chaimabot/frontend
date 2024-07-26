75647ab9a1556b473fdf832ae370922f9e8f715b
chaima <chaima.bounaouara@gmail.com>
2024-07-26 19:16:07 +0200

    edit register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Helmet from "../components/Helmet/Helmet";
+import CommonSection from "../components/UI/common-section/CommonSection";
 import { Container, Row, Col } from "reactstrap";
-import { Link, useNavigate } from "react-router-dom";
-import { useAuth } from "../contexts/AuthContext";
+import { Link } from "react-router-dom";
 import toast, { Toaster } from "react-hot-toast";
+import { useAuth } from "../contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
+import { IconButton } from "@mui/material";
+import PhotoCamera from "@mui/icons-material/PhotoCamera";
+import "../styles/register.css"; // Import the CSS file for custom styles
 
 const Register = () => {
   const [firstname, setFirstname] = useState("");
@@ -12,27 +17,25 @@ const Register = () => {
   const [password, setPassword] = useState("");
   const [tel, setTel] = useState("");
   const [address, setAddress] = useState("");
-  const [role, setRole] = useState("");
   const [birthdate, setBirthdate] = useState("");
   const [image, setImage] = useState("");
-
-  const { register } = useAuth();
   const navigate = useNavigate();
+  const { register, currentUser } = useAuth();
 
   const submitHandler = async (e) => {
     e.preventDefault();
+    const userData = {
+      firstname,
+      lastname,
+      email,
+      password,
+      tel,
+      address,
+      role: "client",
+      birthdate,
+      image,
+    };
     try {
-      const userData = {
-        firstname,
-        lastname,
-        email,
-        password,
-        tel,
-        address,
-        role,
-        birthdate,
-        image,
-      };
       await register(userData);
       toast.success("Account created successfully!");
       navigate("/login");
@@ -41,15 +44,53 @@ const Register = () => {
     }
   };
 
+  useEffect(() => {
+    if (currentUser) {
+      navigate("/profile");
+    }
+  }, [currentUser, navigate]);
+
   return (
     <Helmet title="Signup">
+      <CommonSection title="Signup" />
       <section>
         <Toaster position="top-center" reverseOrder={false} />
         <Container>
           <Row>
             <Col lg="6" md="6" sm="12" className="m-auto text-center">
-              <h3 className="mb-5">Create Account</h3>
+              <h3 className="mb-5">Create an Account</h3>
               <form className="form mb-5" onSubmit={submitHandler}>
+                <div className="image-upload">
+                  <input
+                    type="file"
+                    id="image-upload"
+                    accept="image/*"
+                    onChange={(e) => {
+                      if (e.target.files[0]) {
+                        const reader = new FileReader();
+                        reader.onload = () => setImage(reader.result);
+                        reader.readAsDataURL(e.target.files[0]);
+                      }
+                    }}
+                    style={{ display: "none" }} // Hide the file input
+                  />
+                  <label htmlFor="image-upload" className="image-upload-label">
+                    {image ? (
+                      <img src={image} alt="Profile" className="profile-img" />
+                    ) : (
+                      <div className="upload-icon-container">
+                        <IconButton
+                          color="primary"
+                          aria-label="upload picture"
+                          component="span"
+                        >
+                          <PhotoCamera fontSize="large" />
+                        </IconButton>
+                      </div>
+                    )}
+                  </label>
+                </div>
+
                 <div className="form__group">
                   <input
                     type="text"
@@ -85,7 +126,7 @@ const Register = () => {
                 <div className="form__group">
                   <input
                     type="text"
-                    placeholder="Telephone"
+                    placeholder="Phone"
                     value={tel}
                     onChange={(e) => setTel(e.target.value)}
                   />
@@ -98,14 +139,10 @@ const Register = () => {
                     onChange={(e) => setAddress(e.target.value)}
                   />
                 </div>
-                <div className="form__group">
-                  <input
-                    type="text"
-                    placeholder="Role"
-                    value={role}
-                    onChange={(e) => setRole(e.target.value)}
-                  />
-                </div>
+                <input
+                  type="hidden"
+                  value="client" // Default and not editable
+                />
                 <div className="form__group">
                   <input
                     type="date"
@@ -114,16 +151,8 @@ const Register = () => {
                     onChange={(e) => setBirthdate(e.target.value)}
                   />
                 </div>
-                <div className="form__group">
-                  <input
-                    type="text"
-                    placeholder="Image URL"
-                    value={image}
-                    onChange={(e) => setImage(e.target.value)}
-                  />
-                </div>
                 <button type="submit" className="addTOCart__btn">
-                  Sign Up
+                  Register
                 </button>
               </form>
               <Link to="/login">
